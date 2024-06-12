e34a274cde0d0820cf0b57f5897eebf0d8beaf5e
arbreshaejupi <arbresha.ejupi@student.uni-pr.edu>
2024-06-12 23:15:08 +0200

    Grape structure

---
+++ Grapes App/Grape.swift
@@ -0,0 +1,18 @@
+//
+//  Grape.swift
+//  Grapes App
+//
+//  Created by evlijana shehu on 12/06/2024.
+//
+
+import SwiftUI
+
+struct Grape: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Grape()
+}
