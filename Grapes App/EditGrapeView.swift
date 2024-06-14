c33acd1369fc4abb9992ec6ab5e14ba79aeddac7
arbreshaejupi <arbresha.ejupi@student.uni-pr.edu>
2024-06-14 06:21:27 +0200

    .

---
+++ Grapes App/EditGrapeView.swift
@@ -0,0 +1,18 @@
+//
+//  EditGrapeView.swift
+//  Grapes App
+//
+//  Created by evlijana shehu on 14/06/2024.
+//
+
+import SwiftUI
+
+struct EditGrapeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditGrapeView()
+}
